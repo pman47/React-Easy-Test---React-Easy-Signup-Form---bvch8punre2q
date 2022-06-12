af11450cec0927c442415bc7d6dd91116e7bb604
Manish <manishprajapatim416@gmail.com>
2022-06-12 21:20:56 +0530

    .

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,63 @@
-import React from 'react'
-import '../styles/App.css';
-
+import React, { useState } from "react";
+import "../styles/App.css";
+import { signUpFormValidation } from "../utils/validation";
 
 const App = () => {
-  
+  const [error, setError] = useState({});
+
+  const validate = (event) => {
+    event.preventDefault();
+
+    const errors = signUpFormValidation({
+      email: event.target.email.value,
+      password: event.target.password.value,
+      name: event.target.name.value,
+    });
+    setError(errors);
+  };
+
   return (
-
-  )
-}
-
+    <>
+      <form onSubmit={validate}>
+        <div>
+          <label>
+            Name :
+            <input id="name" type="text" name="name" />
+          </label>
+          {error.name == null || error.name == "" ? null : error.name}
+        </div>
+        <br />
+        <div>
+          <label>
+            Email :
+            <input id="email" type="text" name="email" />
+          </label>
+          {error.email == null || error.email == "" ? null : error.email}
+        </div>
+        <br />
+        <div>
+          <label>
+            Password :
+            <input id="password" type="text" name="password" />
+          </label>
+          {error.password == null || error.password == ""
+            ? null
+            : error.password}
+        </div>
+        <br />
+        <div>
+          <label>
+            Consent :
+            <input id="consent" type="checkbox" name="consent" />
+          </label>
+        </div>
+        <br />
+        <div>
+          <input type="submit" value="Signup" />
+        </div>
+      </form>
+    </>
+  );
+};
 
 export default App;
